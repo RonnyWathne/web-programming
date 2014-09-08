bb74de96e788e1683304f14d1bc8bc41368dee88
Krisztian Balog <krisztian.balog@uis.no>
2014-09-08 13:23:31 +0200

    Changing submit button visibility

--- examples-js/inputcheck/inputcheck.js
+++ examples-js/inputcheck/inputcheck.js
@@ -4,23 +4,34 @@ function checkPasswords() {
     var pw1check = document.getElementById("pw1_check");
     var pw2check = document.getElementById("pw2_check");
     
-
+    var isError = false;
     if (pw1.length > 0) {
         if (pw1.length < 6) {
-            pw1check.innerHTML = "Too short";        
+            pw1check.innerHTML = "Too short";   
+            isError = true;
         }
         else {
             pw1check.innerHTML = "";
         }
         if (pw2.length > 0 && pw1 !== pw2) {
             pw2check.innerHTML = "the two passwords don't match";
+            isError = true;
         }
         else {
-            pw2check.innerHTML = "";
+            pw2check.innerHTML = "";            
         }        
     }
     else {
         pw1check.innerHTML = "";
+    }
+    
+    // show the submit button only if everything is ok
+    var btn = document.getElementById("submitBtn");
+    if (pw1.length > 0 && pw2.length > 0 && !isError) {
+        btn.style.display = "";
+    }
+    else {
+        btn.style.display = "none";
     }
     
 }
