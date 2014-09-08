fb48bc116b5d1afdd69683df8df7ca7ae9a2303f
Krisztian Balog <krisztian.balog@uis.no>
2014-09-08 11:07:57 +0200

    Show-hide example

---
+++ examples-js/showhide/showhide.js
@@ -0,0 +1,9 @@
+function hide(id) {
+    var element = document.getElementById(id);
+    element.style.display = "none";
+}
+
+function show(id) {
+    var element = document.getElementById(id);
+    element.style.display = "";    
+}
