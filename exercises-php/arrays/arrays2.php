186699b1eac0fdf8427239aa152eca6771adb247
Krisztian Balog <krisztian.balog@uis.no>
2014-09-22 12:27:22 +0200

    Another arrays exercise

---
+++ exercises-php/arrays/arrays2.php
@@ -0,0 +1,36 @@
+<?php
+/*
+Arrays exercise 2
+
+2014-09-22
+*/
+
+/*
+Given an array of integers as parameter, find and output the mean and the 
+median of the parameter array.
+- The mean is the average of the numbers. 
+- The median can be found by sorting the numbers and picking the middle one 
+  (e.g., the median of {3, 3, 5, 9, 11} is 5). If there is an even number of 
+  numbers, then there is no single middle value; the median is then computed as 
+  the mean of the two middle values (the median of {1, 5, 7, 9} 
+  is (5 + 7) / 2 = 6).
+
+For example,
+	$numbers = array(1, 5, 7, 9);
+	meanAndMedian($numbers);
+should output
+	mean =  5.5
+	median = 6 
+*/
+
+$numbers = array(1, 5, 7, 9);
+
+function meanAndMedian($numbers) {
+
+	// THIS IS FOR YOU TO COMPLETE
+	
+} 
+
+meanAndMedian($numbers);
+
+?>
